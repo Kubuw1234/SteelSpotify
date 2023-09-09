b55a932ae80a0cc7ba780a7f46dc321ae3eaa78f
Kubek <46279873+Kubuw1234@users.noreply.github.com>
2023-09-10 00:44:54 +0200

    Update and rename dotenv to spotify_integration.py

---
+++ spotify_integration.py
@@ -0,0 +1,12 @@
+# spotify_integration.py
+import requests
+from config import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET
+
+def get_spotify_access_token():
+    # Kod do uzyskania tokena dostępu do Spotify API
+
+def get_current_track_info():
+    # Kod do pobierania informacji o aktualnie granym utworze na Spotify
+
+def create_json_track_info(track_info):
+    # Kod do tworzenia formatu JSON na podstawie informacji o utworze z Spotify
