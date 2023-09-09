47c48b8dbc31629af7b6864364e8453133bcdb63
Kubek <46279873+Kubuw1234@users.noreply.github.com>
2023-09-10 00:45:31 +0200

    Update and rename os.environ to steelseries_integration.py

---
+++ steelseries_integration.py
@@ -0,0 +1,8 @@
+# steelseries_integration.py
+import steelseries
+
+def initialize_sdk():
+    # Kod inicjalizujący połączenie z SDK SteelSeries GG
+
+def update_keyboard_display(track_info):
+    # Kod aktualizujący wyświetlanie na klawiaturze SteelSeries GG
