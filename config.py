1562751ef7ae86e6da87e8df25f46d28fdd7f841
Kubek <46279873+Kubuw1234@users.noreply.github.com>
2023-09-10 00:44:05 +0200

    config file

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
